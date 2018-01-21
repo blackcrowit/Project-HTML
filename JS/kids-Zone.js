501298d3013cc9e505aa8fd2e7351021fe21b9c5
Black Crow <quanghungleo@gmail.com>
2018-01-21 16:32:10 +0700

    asdfasfasfaa

--- JS/kids-Zone.js
+++ JS/kids-Zone.js
@@ -1,18 +1,26 @@
+// $(document).ready(function(){
+//     $(window).scroll(function(){
+//         if($(this).scrollTop() > 50 ) {
+//             $('#main-navbar').css("padding-left", "10%");
+//             $('#main-navbar').css("padding-right", "10%");
+//         }
+//         else {
+//             $('#main-navbar').css("padding-left", "0");
+//             $('#main-navbar').css("padding-right", "0",);
+//         }
+//     });
 
+//     $('#toggle-nav-button').click(function(){
+//         $('#main-navbar').css("background-color", "#FFFFFF");
+//     });
+// });
 
-$(document).ready(function(){
-    $(window).scroll(function(){
-        if($(this).scrollTop() > 50 ) {
-            $('#main-navbar').css("padding-left", "10%");
-            $('#main-navbar').css("padding-right", "10%");
-        }
-        else {
-            $('#main-navbar').css("padding-left", "0");
-            $('#main-navbar').css("padding-right", "0",);
-        }
-    });
+window.onscroll = function() {myFunction()};
 
-    $('#toggle-nav-button').click(function(){
-        $('#main-navbar').css("background-color", "#FFFFFF");
-    });
-});
+function myFunction() {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        document.getElementById("main-navbar").className = "navbar navbar-default navbar-fixed-top box-shadow test";
+    } else {
+        document.getElementById("main-navbar").className = "navbar navbar-default navbar-fixed-top box-shadow";
+    }
+}
