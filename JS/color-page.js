26a18052ea09f560c43e15f9a4c2a0eedafdd808
Black Crow <quanghungleo@gmail.com>
2018-01-31 14:42:01 +0700

    sfdâfâfầ

---
+++ JS/color-page.js
@@ -0,0 +1,59 @@
+var blue = document.getElementById("myBlue"); 
+function playBlue() { 
+    blue.play(); 
+}
+
+var cyan = document.getElementById("myCyan"); 
+function playCyan() { 
+    cyan.play(); 
+}
+
+var green = document.getElementById("myGreen"); 
+function playGreen() { 
+    green.play(); 
+}
+
+var yellow = document.getElementById("myYellow"); 
+function playYellow() { 
+    yellow.play(); 
+}
+
+var red = document.getElementById("myRed"); 
+function playRed() { 
+    red.play(); 
+}
+
+var magenta = document.getElementById("myMagenta"); 
+function playMagenta() { 
+    magenta.play(); 
+}
+
+var purple = document.getElementById("myPurple"); 
+function playPurple() { 
+    purple.play(); 
+}
+
+var orange = document.getElementById("myOrange"); 
+function playOrange() { 
+    orange.play(); 
+}
+
+var black = document.getElementById("myBlack"); 
+function playBlack() { 
+    black.play(); 
+}
+
+var white = document.getElementById("myWhite"); 
+function playWhite() { 
+    white.play(); 
+}
+
+var pink = document.getElementById("myPink"); 
+function playPink() { 
+    pink.play(); 
+}
+
+var brown = document.getElementById("myBrown"); 
+function playBrown() { 
+    brown.play(); 
+}
