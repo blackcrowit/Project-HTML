2dce831c634d816865cde9085102db807d7963bd
Black Crow <quanghungleo@gmail.com>
2018-01-31 23:43:52 +0700

    sffsdfgegewrger

---
+++ JS/js-time.js
@@ -0,0 +1,59 @@
+var t1 = document.getElementById("myt1");
+function playt1() {
+    t1.play();
+}
+
+var t2 = document.getElementById("myt2");
+function playt2() {
+    t2.play();
+}
+
+var t3 = document.getElementById("myt3");
+function playt3() {
+    t3.play();
+}
+
+var t4 = document.getElementById("myt4");
+function playt4() {
+    t4.play();
+}
+
+var t5 = document.getElementById("myt5");
+function playt5() {
+    t5.play();
+}
+
+var t6 = document.getElementById("myt6");
+function playt6() {
+    t6.play();
+}
+
+var t7 = document.getElementById("myt7");
+function playt7() {
+    t7.play();
+}
+
+var t8 = document.getElementById("myt8");
+function playt8() {
+    t8.play();
+}
+
+var t9 = document.getElementById("myt9");
+function playt9() {
+    t9.play();
+}
+
+var t10 = document.getElementById("myt10");
+function playt10() {
+    t10.play();
+}
+
+var t11 = document.getElementById("myt11");
+function playt11() {
+    t11.play();
+}
+
+var t12 = document.getElementById("myt12");
+function playt12() {
+    t12.play();
+}
