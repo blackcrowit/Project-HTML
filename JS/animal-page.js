190fe3aaf592bf5f42ca350a63ac33f65693299a
Black Crow <quanghungleo@gmail.com>
2018-01-31 11:49:33 +0700

    dfsdfdsfsdfsfsfsdfds

---
+++ JS/animal-page.js
@@ -0,0 +1,99 @@
+var tiger = document.getElementById("myTiger"); 
+function playTiger() { 
+    tiger.play(); 
+} 
+
+var camel = document.getElementById("myCamel"); 
+function playCamel() { 
+    camel.play(); 
+}
+
+var bear = document.getElementById("myBear"); 
+function playBear() { 
+    bear.play(); 
+}
+
+var bird = document.getElementById("myBird"); 
+function playBird() { 
+    bird.play(); 
+}
+
+var elephant = document.getElementById("myElephant"); 
+function playElephant() { 
+    elephant.play(); 
+}
+
+var giraffe = document.getElementById("myGiraffe"); 
+function playGiraffe() { 
+    giraffe.play(); 
+}
+
+var hippo = document.getElementById("myHippo"); 
+function playHippo() { 
+    hippo.play(); 
+}
+
+var lion = document.getElementById("myLion"); 
+function playLion() { 
+    lion.play(); 
+}
+
+var monkey = document.getElementById("myMonkey"); 
+function playMonkey() { 
+    monkey.play(); 
+}
+
+var octopus = document.getElementById("myOctopus"); 
+function playOctopus() { 
+    octopus.play(); 
+}
+
+var panther = document.getElementById("myPanther"); 
+function playPanther() { 
+    panther.play(); 
+}
+
+var penguins = document.getElementById("myPenguins"); 
+function playPenguins() { 
+    penguins.play(); 
+}
+
+var polarBear = document.getElementById("myPolarBear"); 
+function playPolarBear() { 
+    polarBear.play(); 
+}
+
+var rhino = document.getElementById("myRhino"); 
+function playRhino() { 
+    rhino.play(); 
+}
+
+var seahorse = document.getElementById("mySeaHorse"); 
+function playSeaHorse() { 
+    seahorse.play(); 
+}
+
+var snake = document.getElementById("mySnake"); 
+function playSnake() { 
+    snake.play(); 
+}
+
+var tortoise = document.getElementById("myTortoise"); 
+function playTortoise() { 
+    tortoise.play(); 
+}
+
+var turtle = document.getElementById("myTurtle"); 
+function playTurtle() { 
+    turtle.play(); 
+}
+
+var whale = document.getElementById("myWhale"); 
+function playWhale() { 
+    whale.play(); 
+}
+
+var zebra = document.getElementById("myZebra"); 
+function playZebra() { 
+    zebra.play(); 
+}
