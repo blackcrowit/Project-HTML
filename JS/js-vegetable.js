efd85218003f93033c7a8d34792d49a326327b99
Cuong <cuongdmd00625@fpt.edu.vn>
2018-02-01 11:45:43 +0700

    fsdfdsf

---
+++ JS/js-vegetable.js
@@ -0,0 +1,125 @@
+var q1 = document.getElementById("myq1");
+function playq1() {
+    q1.play();
+}
+
+var q2 = document.getElementById("myq2");
+function playq2() {
+    q2.play();
+}
+
+var q3 = document.getElementById("myq3");
+function playq3() {
+    q3.play();
+}
+
+var q4 = document.getElementById("myq4");
+function playq4() {
+    q4.play();
+}
+
+
+var q5 = document.getElementById("myq5");
+function playq5() {
+    q5.play();
+}
+
+var q6 = document.getElementById("myq6");
+function playq6() {
+    q6.play();
+}
+
+var q7 = document.getElementById("myq7");
+function playq7() {
+    q7.play();
+}
+
+var q8 = document.getElementById("myq8");
+function playq8() {
+    q8.play();
+}
+
+var q9 = document.getElementById("myq9");
+function playq9() {
+    q9.play();
+}
+
+var q10 = document.getElementById("myq10");
+function playq10() {
+    q10.play();
+}
+
+var q11 = document.getElementById("myq11");
+function playq11() {
+    q11.play();
+}
+
+var q12 = document.getElementById("myq12");
+function playq12() {
+    q12.play();
+}
+
+var q13 = document.getElementById("myq13");
+function playq13() {
+    q13.play();
+}
+
+var q14 = document.getElementById("myq14");
+function playq14() {
+    q14.play();
+}
+
+var q15 = document.getElementById("myq15 ");
+function playq15 () {
+    q15 .play();
+}
+
+var q16 = document.getElementById("myq16");
+function playq16() {
+    q16.play();
+}
+
+var q17 = document.getElementById("myq17");
+function playq17() {
+    q17.play();
+}
+
+var q18 = document.getElementById("myq18");
+function playq18() {
+    q18.play();
+}
+
+var q19 = document.getElementById("myq19");
+function playq19() {
+    q19.play();
+}
+
+var q20 = document.getElementById("myq20");
+function playq20() {
+    q20.play();
+}
+
+var q21 = document.getElementById("myq21");
+function playq21() {
+    q21.play();
+}
+var q22 = document.getElementById("myq22");
+function playq22() {
+    q22.play();
+}
+
+
+var q23 = document.getElementById("myq23");
+function playq23() {
+    q23.play();
+}
+
+var q24 = document.getElementById("myq24");
+function playq24() {
+    q24.play();
+}
+
+var q25 = document.getElementById("myq25");
+function playq25() {
+    q25.play();
+}
