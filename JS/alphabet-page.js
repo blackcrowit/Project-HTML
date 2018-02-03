25c18ac2cecd5b20aad576e4731637d398146b0a
Black Crow <quanghungleo@gmail.com>
2018-02-03 17:11:49 +0700

    fsasfasfasfas

---
+++ JS/alphabet-page.js
@@ -0,0 +1,196 @@
+
+// for (var i = 0; i <= 26; i++;) {
+// 	a[i] = document.getElementById("myq"+i);
+// 	function playai() {
+// 	    a[i].play();
+// 	}	    
+// }
+
+// let a = [];
+// for (i=0; i < 10; i++) {
+//   let qi = document.getElementById('myq' + i);
+//   let handle = function() {
+//     qi.play();
+//   }
+//   a.push({qi, handle})
+// }
+
+var a1 = document.getElementById("mya1");
+function playa1() {
+    a1.play();
+}
+
+var a2 = document.getElementById("mya2");
+function playa2() {
+    a2.play();
+}
+
+var a3 = document.getElementById("mya3");
+function playa3() {
+    a3.play();
+}
+
+var a4 = document.getElementById("mya4");
+function playa4() {
+    a4.play();
+}
+
+var a5 = document.getElementById("mya5");
+function playa5() {
+    a5.play();
+}
+
+var a6 = document.getElementById("mya6");
+function playa6() {
+    a6.play();
+}
+
+var a7 = document.getElementById("mya7");
+function playa7() {
+    a7.play();
+}
+
+var a8 = document.getElementById("mya8");
+function playa8() {
+    a8.play();
+}
+
+var a9 = document.getElementById("mya9");
+function playa9() {
+    a9.play();
+}
+
+var a10 = document.getElementById("mya10");
+function playa10() {
+    a10.play();
+}
+
+var a11 = document.getElementById("mya11");
+function playa11() {
+    a11.play();
+}
+
+var a12 = document.getElementById("mya12");
+function playa12() {
+    a12.play();
+}
+
+var a13 = document.getElementById("mya13");
+function playa13() {
+    a13.play();
+}
+
+var a14 = document.getElementById("mya14");
+function playa14() {
+    a14.play();
+}
+
+var a15 = document.getElementById("mya15");
+function playa15() {
+    a15.play();
+}
+
+var a16 = document.getElementById("mya16");
+function playa16() {
+    a16.play();
+}
+
+var a17 = document.getElementById("mya17");
+function playa17() {
+    a17.play();
+}
+
+var a18 = document.getElementById("mya18");
+function playa18() {
+    a18.play();
+}
+
+var a19 = document.getElementById("mya19");
+function playa19() {
+    a19.play();
+}
+
+var a20 = document.getElementById("mya20");
+function playa20() {
+    a20.play();
+}
+
+var a21 = document.getElementById("mya21");
+function playa21() {
+    a21.play();
+}
+
+var a22 = document.getElementById("mya22");
+function playa22() {
+    a22.play();
+}
+
+var a23 = document.getElementById("mya23");
+function playa23() {
+    a23.play();
+}
+
+var a24 = document.getElementById("mya24");
+function playa24() {
+    a24.play();
+}
+
+var a25 = document.getElementById("mya25");
+function playa25() {
+    a25.play();
+}
+
+var a26 = document.getElementById("mya26");
+function playa26() {
+    a26.play();
+}
+
+var n1 = document.getElementById("myn1");
+function playn1() {
+    n1.play();
+}
+
+var n2 = document.getElementById("myn2");
+function playn2() {
+    n2.play();
+}
+
+var n3 = document.getElementById("myn3");
+function playn3() {
+    n3.play();
+}
+
+var n4 = document.getElementById("myn4");
+function playn4() {
+    n4.play();
+}
+
+var n5 = document.getElementById("myn5");
+function playn5() {
+    n5.play();
+}
+
+var n6 = document.getElementById("myn6");
+function playn6() {
+    n6.play();
+}
+
+var n7 = document.getElementById("myn7");
+function playn7() {
+    n7.play();
+}
+
+var n8 = document.getElementById("myn8");
+function playn8() {
+    n8.play();
+}
+
+var n9 = document.getElementById("myn9");
+function playn9() {
+    n9.play();
+}
+
+var n10 = document.getElementById("myn10");
+function playn10() {
+    n10.play();
+}
